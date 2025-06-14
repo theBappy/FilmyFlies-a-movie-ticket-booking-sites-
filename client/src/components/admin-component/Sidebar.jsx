14e8b93196cf2b490f719d45b7c65524f5cdbe91
theBappy <abappy575@gmail.com>
2025-06-14 15:01:35 +0600

    admin ui for dashboard, show list of bookings, adding a movie, set the price etc

--- client/src/components/admin-component/Sidebar.jsx
+++ client/src/components/admin-component/Sidebar.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import {
+  LayoutDashboardIcon,
+  ListCollapseIcon,
+  ListIcon,
+  PlusSquareIcon,
+} from "lucide-react";
+import { assets } from "../../assets/assets";
+import { NavLink } from "react-router-dom";
 
-const Sidebar = () => {
+const AdminSidebar = () => {
+  const user = {
+    firstName: "Admin",
+    lastName: "User",
+    imageUrl: assets.profile,
+  };
+
+  const adminNavLinks = [
+    { name: "Dashboard", path: "/admin", icon: LayoutDashboardIcon },
+    { name: "Add Shows", path: "/admin/add-shows", icon: PlusSquareIcon },
+    { name: "List Shows", path: "/admin/list-shows", icon: ListIcon },
+    {
+      name: "List Bookings",
+      path: "/admin/list-bookings",
+      icon: ListCollapseIcon,
+    },
+  ];
+
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="h-[calc(100vh-64px)] md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-gray-300/20 text-sm">
+      <img
+        src={user.imageUrl}
+        alt="user-image"
+        className="h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto"
+      />
+      <p className="mt-2 text-base max-md:hidden">
+        {user.firstName} {user.lastName}
+      </p>
+      <div className="w-full">
+        {adminNavLinks.map((link, index) => (
+          <NavLink
+            to={link.path} end
+            className={({ isActive }) =>
+              `relative flex items-center max-md:justify-center gap-2 w-full py-2.5 min-md:pl-10 first:mt-6 text-gray-400 ${
+                isActive && "bg-primary/15 text-primary group"
+              }`
+            }
+            key={index}
+          >
+            {({ isActive }) => (
+              <>
+                <link.icon className="w-5 h-5" />
+                <p className="max-md:hidden">{link.name}</p>
+                <span
+                  className={`w-1.5 h-10 rounded-lg right-0 absolute ${
+                    isActive && "bg-primary"
+                  }`}
+                />
+              </>
+            )}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default AdminSidebar;
--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { dummyShowsData } from "../../assets/assets";
+import Loading from "../../components/Loading";
+import { Title } from "../../components/admin-component/Title";
+import { CheckIcon, StarIcon } from "lucide-react";
+import { KConverter } from "../../lib/KConverter";
 
 const AddShows = () => {
-  return (
-    <div>AddShows</div>
-  )
-}
+  const currency = import.meta.env.VITE_CURRENCY;
+  const [nowPlayingMovies, seNowPlayingMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [dateTimeSelection, setDateTimeSelection] = useState({});
+  const [dateTimeInput, setDateTimeInput] = useState("");
+  const [showPrice, setShowPrice] = useState("");
 
-export default AddShows
+  const fetchNowPlayingMovies = async () => {
+    seNowPlayingMovies(dummyShowsData);
+  };
+  useEffect(() => {
+    fetchNowPlayingMovies();
+  }, []);
+
+  return nowPlayingMovies.length > 0 ? (
+    <>
+      <Title text1="Add" text2="Movies" />
+      <p className="mt-10 text-lg font-medium">Now Playing Movies</p>
+      <div className="overflow-x-auto pb-4">
+        <div className="group flex flex-wrap gap-4 mt-4 w-max">
+          {nowPlayingMovies.map((movie) => (
+            <div
+              key={movie.id}
+              className={`relative max-w-40 cursor-pointer group-hover:not-hover:opacity-40 hover:-translate-y-1 transition duration-300`}
+              onClick={() => setSelectedMovie(movie.id)}
+            >
+              <div className="relative rounded-lg overflow-hidden">
+                <img
+                  src={movie.poster_path}
+                  alt="poster"
+                  className="w-full object-cover brightness-90"
+                />
+                <div className="text-sm flex items-center justify-between p-2 bg-black/70 w-full absolute bottom-0 left-0">
+                  <p className="flex items-center gap-1 text-gray-400">
+                    <StarIcon className="w-4 h-4 text-primary fill-primary" />
+                    {movie.vote_average.toFixed(1)}
+                  </p>
+                  <p className="text-gray-300">{KConverter(movie.vote_count)} Votes</p>
+                </div>
+              </div>
+              {selectedMovie === movie.id && (
+                <div className="absolute top-2 right-2 flex items-center justify-center bg-primary h-6 w-6 rounded">
+                    <CheckIcon className="w-4 h-4 text-white" strokeWidth={2.5} />
+                </div>
+              )}
+              <p className="font-medium truncate">{movie.title}</p>
+              <p className="text-gray-400 text-sm">{movie.release_date}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* show price input */}
+      <div className="mt-8">
+          <label className="block text-sm font-medium mb-2">Show Price</label>
+          <div className="inline-flex items-center gap-2 border border-gray-600 px-3 py2 rounded-md">
+            <p className="text-gray-400 text-sm">
+                {currency}
+            </p>
+            <input type="number" min={0} value={showPrice} onChange={(e) => setShowPrice(e.target.value)} placeholder="Enter each ticket price" className="outline-none" />
+          </div>
+      </div>
+    </>
+  ) : (
+    <Loading />
+  );
+};
+
+export default AddShows;
