7de066340877d6a6e2adcb43780fc59250b89f31
theBappy <abappy575@gmail.com>
2025-06-15 12:23:45 +0600

    MongoDB and Inngest initiliation for database management

---
+++ server/config/db.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log('✅ Mongoose.connect resolved.');
+
+    // Important: lowercase 'connected'
+    mongoose.connection.on('connected', () => {
+      console.log('✅ Database connected successfully');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      console.error('❌ Mongoose connection error:', err);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      console.log('⚠️ Mongoose disconnected');
+    });
+  } catch (error) {
+    console.error('❌ DB Connection Failed:', error.message);
+  }
+};
+
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,29 @@
+import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import 'dotenv/config'
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from './inngest/index.js';
+import connectDB from './config/db.js'
+
 
 const app = express()
-
 const PORT = process.env.PORT || 3000
 
-// middlewares
+// Middlewares
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
-
-// api routes
-app.get('/', (req,res) =>{
-    res.send('Hello World')
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World')
 })
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.listen(PORT, ()=> console.log(`Server is listening at http://localhost:${PORT}`))
+// ✅ First connect to DB, then start server
+await connectDB()
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is listening at http://localhost:${PORT}`)
+})
