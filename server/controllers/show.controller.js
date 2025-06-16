432214af736fda7ac710d64843682a7b1489177c
theBappy <abappy575@gmail.com>
2025-06-17 00:05:10 +0600

    Notification email for new added show wiht inngest trigger function

--- server/controllers/show.controller.js
+++ server/controllers/show.controller.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import Movie from "../models/movie.model.js";
 import Show from "../models/shows.model.js";
+import { inngest } from "../inngest/index.js";
 
 export const getNowPlayingMovies = async (req, res) => {
   try {
@@ -78,6 +79,11 @@ export const addShow = async (req, res) => {
     if (showsToCreate.length > 0) {
       await Show.insertMany(showsToCreate);
     }
+    // trigger inngest event
+    await inngest.send({
+      name: 'app/show.added',
+      data: {movieTitle: movie.title}
+    })
 
     res.json({ success: true, message: "Show added successfully" });
 
